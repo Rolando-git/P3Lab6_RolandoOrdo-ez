af86e970d87e2da7e4edbcd6c339c50a1ee649f8
Rolando <roloordonez2001@hotmail.com>
2021-11-19 13:36:39 -0600

    "..."

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <vector>
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
-using std::vector;
 
 int main(){
-    cout << "Hola" << endl;
+    
     return 0;
 }
